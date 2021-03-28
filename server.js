9d15b59a3e428552cff253c4b432a988d90ae6e0
Charu Sharma <61303893+CharuSharma13@users.noreply.github.com>
2021-03-28 22:24:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -41,29 +41,7 @@ app.post("/api",function(req,res){
 
 })
 
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static('frontend/build'));
-// Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
-  });
-}
 
-const whitelist = ['http://localhost:3000'​, 'http://localhost:8080'​, 'https://shrouded-journey-38552.heroku...​]
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("** Origin of request " + origin)
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable")
-      callback(null, true)
-    } else {
-      console.log("Origin rejected")
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-app.use(cors(corsOptions))
 
 app.listen(PORT,function(){
 	console.log("running on port 4000");
