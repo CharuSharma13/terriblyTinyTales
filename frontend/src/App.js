a77f99f3f3a43197d2d617be7c4a3f35b8ea5a51
Charu Sharma <61303893+CharuSharma13@users.noreply.github.com>
2021-05-31 13:11:02 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,31 +2,26 @@ import './App.css';
 import React,{useState,useEffect} from 'react';
 import axios from 'axios';
 function App() {
-    const url="/login";
-    const allRoll=[]
+    
     const [data,setData]=useState({
         name:"",roll:""
     })
-    const [data1,setData1]=useState({
-        name:"",roll:""
-    })
+    
     const [state,setState]=useState([])
-    const [print,setPrint]=useState("");
+    
 
     function handle(e){
         const newdata={...data}
         newdata[e.target.id]=e.target.value
         setData(newdata)
-        
-       
 
-        
     }
+
 
     function submit(e){
         e.preventDefault();
         var OPTIONS = {
-            url: "http://localhost:4000/api",
+            url: "https://glacial-fjord-33512.herokuapp.com/api/",
             method: "POST",
             data:{name:data.name},
             headers: {
@@ -42,14 +37,15 @@ function App() {
 
     
     function detail(res){
-        // setPrint(res.data.message);
+        
         console.log(res.data)
-        document.getElementById('roll').innerHTML=res.data.input; 
-        document.getElementById('status').innerHTML=res.data.message; 
-         // setState((items)=>{
-         //    console.log(...items);
-         //    return [...items,print];
-         // })    
+        
+        let result={
+          inp:res.data.input,
+          mes:res.data.message
+        }
+        setState(prev=>[...prev,result]);
+         
 
            
     }
@@ -58,6 +54,7 @@ function App() {
 
     
   return (
+    <>
     <div className="centerDiv">
     
     
@@ -65,21 +62,33 @@ function App() {
     <form onSubmit={(e)=>submit(e)}>
     <input id="name" type="text" onChange={(e)=>handle(e)}  />
     <button type="submit" >Submit</button>
-    <table id="details">
-   
-  <tr>
-    <th>Roll-Number</th>
-    <th>Status</th>
-  </tr>
-  <tr>
-    <td id="roll"></td>
-    <td id="status"></td>
-   
-  </tr>
-    </table>
+    
+   {
+    state.length > 0
+                &&
+                <table id="details">
+                    <tr>
+                      <th>Roll-Number</th>
+                      <th>Status</th>
+                    </tr>
+                    {
+                        state.map((val,i)=>{
+                            return (<tr key={i}>
+                            <td >{val.inp}</td>
+                            <td >{val.mes}</td>
+                            </tr>);
+                          })
+                    }
+                </table>
+    
+   }
+ 
+  
+    
     </form>
     
     </div>
+    </>
   );
 }
 
